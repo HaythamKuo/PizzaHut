3bc976e544a93e37bbfb3215a92386c9cecccbe0
Haytham <qaz7954200@livemail.tw>
2023-07-20 15:07:12 +0800

    load recipes and fix bugs

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # PizzaHut
+
+# Intro
+
+1. 僅用來練習與展示 並無任何商用目的
+
+# Get started
+
+`npm intstall`
+`npm run dev`
--- main.js
+++ main.js
@@ -2,59 +2,67 @@ import "./style.css";
 import "./styles/main.scss";
 
 import axios from "axios";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.js";
-import { Logger } from "sass";
+import icons from "./public/img/icons.svg";
+//再build application之後路徑可能有所改變
+//import icons from "url:./public/img/icons.svg";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
-// document.querySelector("#app").innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//     <div class="test"></div>
-//   </div>
-// `;
-
-// setupCounter(document.querySelector("#counter"));
+const renderSpinner = function (element) {
+  const markup = `
+  <div class="spinner">
+  <svg>
+    <use href="${icons}#icon-loader"></use>
+  </svg>
+</div>
+  `;
+  element.innerHTML = "";
+  element.insertAdjacentHTML("afterbegin", markup);
+};
 
 //將主要模板導入進來
-const testfetch = async () => {
+const testFetch = async () => {
   try {
     const response = await axios.get("main.html");
-    const data = response.data;
+
     const testEl = document.querySelector("#app");
-    testEl.innerHTML = data;
+    testEl.innerHTML = response.data;
+
+    showRecipe();
   } catch (error) {
-    console.error("Error fetching main.html:", error);
+    console.error("找不到 main.html:", error);
   }
 };
 
-testfetch();
+// const recipeContainer = document.querySelector(".recipe");
+// const timeout = function (second) {
+//   return new Promise((_, reject) => {
+//     setTimeout(function () {
+//       reject(new Error(`獲取資料時間過久，已經過了${second}秒`));
+//     }, second * 1000);
+//   });
+// };
 
+testFetch();
+
+//渲染食譜方法////////////////////
 const showRecipe = async function () {
   try {
-    const res = await fetch(
-      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+    //動態取得菜單id
+    const id = window.location.hash.slice(1);
+    console.log(id);
+    if (!id) return;
+
+    const res = await axios.get(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    console.log(data);
-    let { recipe } = data.data;
+    if (res.status !== 200) throw new Error(`${data.message} (${res.status})`);
+
+    let { recipe } = res.data.data;
     recipe = {
       cookingTime: recipe.cooking_time,
       id: recipe.id,
-      imageUrl: recipe.image_url,
+      image: recipe.image_url,
       ingredients: recipe.ingredients,
       publisher: recipe.publisher,
       servings: recipe.servings,
@@ -62,9 +70,114 @@ const showRecipe = async function () {
       title: recipe.title,
     };
     console.log(recipe);
+    const recipeContainer = document.querySelector(".recipe");
+    renderSpinner(recipeContainer);
+    const timeout = function (second) {
+      return new Promise((_, reject) => {
+        setTimeout(function () {
+          reject(new Error(`獲取資料時間過久，已經過了${second}秒`));
+        }, second * 1000);
+      });
+    };
+
+    // 渲染菜單////////////////////////////
+    const markup = ` <figure class="recipe__fig">
+    <img src="${recipe.image}" alt="${recipe.title}" class="recipe__img" />
+    <h1 class="recipe__title">
+      <span>${recipe.title}</span>
+    </h1>
+  </figure>
+
+  <div class="recipe__details">
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}#icon-clock"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--minutes">${
+        recipe.cookingTime
+      }</span>
+      <span class="recipe__info-text">minutes</span>
+    </div>
+    <div class="recipe__info">
+      <svg class="recipe__info-icon">
+        <use href="${icons}#icon-users"></use>
+      </svg>
+      <span class="recipe__info-data recipe__info-data--people">${
+        recipe.servings
+      }</span>
+      <span class="recipe__info-text">servings</span>
+
+      <div class="recipe__info-buttons">
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icons}#icon-minus-circle"></use>
+          </svg>
+        </button>
+        <button class="btn--tiny btn--increase-servings">
+          <svg>
+            <use href="${icons}#icon-plus-circle"></use>
+          </svg>
+        </button>
+      </div>
+    </div>
+
+    <div class="recipe__user-generated">
+      <svg>
+        <use href="${icons}#icon-user"></use>
+      </svg>
+    </div>
+    <button class="btn--round">
+      <svg class="">
+        <use href="${icons}#icon-bookmark-fill"></use>
+      </svg>
+    </button>
+  </div>
+
+  <div class="recipe__ingredients">
+    <h2 class="heading--2">Recipe ingredients</h2>
+    <ul class="recipe__ingredient-list">
+    ${recipe.ingredients
+      .map((ing) => {
+        return ` <li class="recipe__ingredient">
+      <svg class="recipe__icon">
+        <use href="${icons}#icon-check"></use>
+      </svg>
+      <div class="recipe__quantity">${ing.quantity}</div>
+      <div class="recipe__description">
+        <span class="recipe__unit">${ing.unit}</span>
+        ${ing.description}
+      </div>
+    </li>`;
+      })
+      .join("")}
+    </ul>
+  </div>
+
+  <div class="recipe__directions">
+    <h2 class="heading--2">How to cook it</h2>
+    <p class="recipe__directions-text">
+      This recipe was carefully designed and tested by
+      <span class="recipe__publisher">${
+        recipe.publisher
+      }</span>. Please check out
+      directions at their website.
+    </p>
+    <a
+      class="btn--small recipe__btn"
+      href="${recipe.sourceUrl}"
+      target="_blank"
+    >
+      <span>Directions</span>
+      <svg class="search__icon">
+        <use href="${icons}#icon-arrow-right"></use>
+      </svg>
+    </a>
+  </div>`;
+    recipeContainer.innerHTML = "";
+    recipeContainer.insertAdjacentHTML("afterbegin", markup);
   } catch (error) {
     alert(error);
   }
 };
 
-showRecipe();
+["hashchange", "load"].forEach((e) => window.addEventListener(e, showRecipe));
